5fdf9d9e23789a67b7f21b7d83151b0e3cd3468c
Vitor Hugo Bitencourt <vitorbitencourt8486@gmail.com>
2022-01-12 10:04:13 -0300

    Projeto de Reajuste Salarial

---
+++ ReajusteSalarial/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace ReajusteSalarial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_nome = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_rj = new System.Windows.Forms.TextBox();
+            this.txt_cat2 = new System.Windows.Forms.TextBox();
+            this.txt_nome2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_cat = new System.Windows.Forms.TextBox();
+            this.txt_sal = new System.Windows.Forms.TextBox();
+            this.btn_limpar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome:";
+            // 
+            // txt_nome
+            // 
+            this.txt_nome.Location = new System.Drawing.Point(84, 30);
+            this.txt_nome.Name = "txt_nome";
+            this.txt_nome.Size = new System.Drawing.Size(100, 20);
+            this.txt_nome.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_rj);
+            this.groupBox1.Controls.Add(this.txt_cat2);
+            this.groupBox1.Controls.Add(this.txt_nome2);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(12, 271);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(270, 167);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // txt_rj
+            // 
+            this.txt_rj.Location = new System.Drawing.Point(151, 111);
+            this.txt_rj.Name = "txt_rj";
+            this.txt_rj.Size = new System.Drawing.Size(100, 20);
+            this.txt_rj.TabIndex = 8;
+            // 
+            // txt_cat2
+            // 
+            this.txt_cat2.Location = new System.Drawing.Point(94, 68);
+            this.txt_cat2.Name = "txt_cat2";
+            this.txt_cat2.Size = new System.Drawing.Size(100, 20);
+            this.txt_cat2.TabIndex = 8;
+            // 
+            // txt_nome2
+            // 
+            this.txt_nome2.Location = new System.Drawing.Point(72, 29);
+            this.txt_nome2.Name = "txt_nome2";
+            this.txt_nome2.Size = new System.Drawing.Size(100, 20);
+            this.txt_nome2.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(20, 109);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(125, 20);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Salário Reajustado:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(18, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Categoria:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(18, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Nome:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(67, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Calcular Reajuste";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Categoria:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(36, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Salário:";
+            // 
+            // txt_cat
+            // 
+            this.txt_cat.Location = new System.Drawing.Point(112, 72);
+            this.txt_cat.Name = "txt_cat";
+            this.txt_cat.Size = new System.Drawing.Size(72, 20);
+            this.txt_cat.TabIndex = 6;
+            // 
+            // txt_sal
+            // 
+            this.txt_sal.Location = new System.Drawing.Point(96, 123);
+            this.txt_sal.Name = "txt_sal";
+            this.txt_sal.Size = new System.Drawing.Size(100, 20);
+            this.txt_sal.TabIndex = 7;
+            // 
+            // btn_limpar
+            // 
+            this.btn_limpar.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_limpar.Location = new System.Drawing.Point(67, 212);
+            this.btn_limpar.Name = "btn_limpar";
+            this.btn_limpar.Size = new System.Drawing.Size(161, 29);
+            this.btn_limpar.TabIndex = 8;
+            this.btn_limpar.Text = "Limpar";
+            this.btn_limpar.UseVisualStyleBackColor = true;
+            this.btn_limpar.Click += new System.EventHandler(this.btn_limpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 450);
+            this.Controls.Add(this.btn_limpar);
+            this.Controls.Add(this.txt_sal);
+            this.Controls.Add(this.txt_cat);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txt_nome);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_nome;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txt_rj;
+        private System.Windows.Forms.TextBox txt_cat2;
+        private System.Windows.Forms.TextBox txt_nome2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_cat;
+        private System.Windows.Forms.TextBox txt_sal;
+        private System.Windows.Forms.Button btn_limpar;
+    }
+}
+
---
+++ ReajusteSalarial/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReajusteSalarial
+{
+    public partial class Form1 : Form
+    {
+        double salario;
+        string nome, cat;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            nome = txt_nome.Text;
+            cat = txt_cat.Text;
+            salario = double.Parse(txt_sal.Text);
+
+            if (cat == "B" || cat == "D" || cat == "E" || cat == "I" || cat == "J" || cat == "J" || cat == "T")
+            {
+                salario = (15 * salario) / 100 + salario;
+            }
+            else if (cat == "R" || cat == "K")
+            {
+                salario = (25 * salario) / 100 + salario;
+            }
+            else if (cat == "L" || cat == "M" || cat == "N" || cat == "O" || cat== "P" || cat == "Q" || cat == "S")
+            {
+                salario = (35 * salario) / 100 + salario;
+            }
+            else
+            {
+                salario = (50 * salario) / 100 + salario;
+            }
+            txt_nome2.Text = Convert.ToString(nome);
+            txt_cat2.Text = Convert.ToString(cat);
+            txt_rj.Text = Convert.ToString(salario.ToString("C"));
+        }
+
+        private void btn_limpar_Click(object sender, EventArgs e)
+        {
+            txt_cat.Clear();
+            txt_cat2.Clear();
+            txt_nome.Clear();
+            txt_nome2.Clear();
+            txt_sal.Clear();
+            txt_rj.Clear();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
